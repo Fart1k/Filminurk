b8d3b8f99a89ebf117a9d865dd4c5e325f7d279d
Opilane TTHK <opilane@tthk.ee>
2025-10-10 12:33:38 +0300

    2
    Services

---
+++ Filminurk.Core/Dto/MoviesDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Dto
+{
+    internal class MovieDTO
+    {
+        public Guid? ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateOnly? FirstPublished { get; set; }
+        public string? Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public double? CurrentRating { get; set; }
+
+        // 3 minu andme
+
+        public Genre? Genre { get; set; }
+
+        // DB jaoks vajalikud
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
