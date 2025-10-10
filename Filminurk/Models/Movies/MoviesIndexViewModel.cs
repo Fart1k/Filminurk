c247961a3779cb3fe2809a96a125c524ae97133c
opilane <opilane@HP-CZC321706P>
2025-10-10 11:50:43 +0300

    Add project files.

---
+++ Filminurk/Models/Movies/MoviesIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Models.Movies
+{
+    public class MoviesIndexViewModel : Controller
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public double? CurrentRating { get; set; }
+
+        public string? Warnings { get; set; }
+    }
+}
