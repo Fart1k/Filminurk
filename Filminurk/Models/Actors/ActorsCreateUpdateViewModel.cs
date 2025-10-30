0aebeba581c9a2bddbb3acc0b23047c3e7abcf91
opilane <opilane@HP-CZC321706P>
2025-10-30 10:45:21 +0200

    Created CreateUpdate View for Actors Modified ViewModel

--- Filminurk/Models/Actors/ActorsCreateUpdateViewModel.cs
+++ Filminurk/Models/Actors/ActorsCreateUpdateViewModel.cs
@@ -5,7 +5,7 @@ namespace Filminurk.Models.Actors
 {
     public class ActorsCreateUpdateViewModel : Controller
     {
-        public Guid ActorID { get; set; }
+        public Guid? ActorID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? NickName { get; set; }
