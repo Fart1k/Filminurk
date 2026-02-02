8b734642a2567f74f7fa3e3503fd1cd9bcfc3d79
Artur Šein <seinartur@gmail.com>
2026-02-02 17:12:29 +0200

    Split to fix a bug

---
+++ Filminurk.Core/Dto/OmdbDTOs/OmdbRatingDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Dto.OmdbDTOs
+{
+    public class OmdbRatingDTO
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+}
