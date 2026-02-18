6ead40bfb2d472e7df587fd7a9be7cb26d853e18
Artur Šein <seinartur@gmail.com>
2026-02-18 13:38:57 +0200

    Added DTO for movie import

---
+++ Filminurk.Core/Dto/OmdbDTOs/OmdbImportMovieDTO.cs
@@ -0,0 +1,18 @@
+﻿using Filminurk.Core.Domain;
+
+namespace Filminurk.Core.Dto.OmdbDTOs
+{
+    public class OmdbImportMovieDTO
+    {
+        public Guid? Id { get; set; }
+        public string Title { get; set; }
+        public DateOnly? FirstPublished { get; set; }
+        public Genre Genre { get; set; }
+        public string Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public string? Description { get; set; }
+        public double? CurrentRating { get; set; }
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
