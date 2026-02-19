9833bacf5f15262b1988eed3f5f4ced944e81eed
Artur Šein <seinartur@gmail.com>
2026-02-19 10:51:47 +0200

    fixed hub. Now its displays username, not user email

--- Filminurk/Hubs/ChatHub.cs
+++ Filminurk/Hubs/ChatHub.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Filminurk.Core.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Filminurk.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChatHub(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public async Task SendMessage(string message)
         {
-            var user = Context.User?.Identity?.Name ?? "Unknown User";
+            var user = await _userManager.GetUserAsync(Context.User);
 
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            var userName = user != null ? user.DisplayName : "Unknown User";
+
+            await Clients.All.SendAsync("ReceiveMessage", userName, message);
         }
     }
 }
