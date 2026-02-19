e7c5a7731d95bfc35b906abbac73ab0ddfe99f87
Artur Šein <seinartur@gmail.com>
2026-02-19 10:45:37 +0200

    Added chat controller

---
+++ Filminurk/Controllers/SignalRChatController.cs
@@ -0,0 +1,28 @@
+﻿using Filminurk.Core.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filminurk.Controllers
+{
+    public class SignalRChatController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        
+        public SignalRChatController(SignInManager<ApplicationUser> signInManager) 
+        { 
+            _signInManager = signInManager; 
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+    }
+}
